91f793a58ccc969ee836a2fe93448193bf426a32
alu0100543674 <Carla Hernandez>
2013-10-25 22:21:42 +0100

    Metodo reduccion definido

--- lib/fracciones.rb
+++ lib/fracciones.rb
@@ -1,7 +1,13 @@
+require "gcd.rb"
 class Fraccion
 attr_reader :numerador, :denominador
    def initialize(numerador, denominador)
      @numerador, @denominador = numerador, denominador
+   end
+   def reduccion()
+      mcd = gcd(@numerador, @denominador)
+      f2 = Fraccion.new(@numerador/mcd, @denominador/mcd)
+      f2
    end
 end
 
