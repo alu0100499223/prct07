911e456c2b0976f4991794bae321b9f5944ebd82
alu0100543674 <Carla Hernandez>
2013-10-25 22:17:17 +0100

    ERRO. No creada el metodo numerador y denominador

--- spec/fracciones_spec.rb
+++ spec/fracciones_spec.rb
@@ -1,1 +1,22 @@
 require "lib/fracciones.rb"
+
+describe Fraccion do
+  before :each do
+    @num = Fraccion.new(2,4)
+  end 
+
+  # En este describe se hace:
+  # Debe existir un numerador
+  # Debe existir un denominador
+  # Pero también:
+  # Se debe invocar al metodo num() para obtener el numerador
+  # Se debe invocar al metodo denom() para obtener el denominador
+  describe " Almacenamiento numerador y denominador" do
+     it "Debe existir un numerador" do
+        @num.numerador.should eq(2)
+     end
+     it "Debe existir un denominador" do
+        @num.denominador.should eq(4)
+     end
+  end
+end
